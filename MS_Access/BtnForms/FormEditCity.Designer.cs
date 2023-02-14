24f4a385122010e0a4173586395ead8e00e19779
Mher <mhero.haxverdyan@gmail.com>
2023-02-14 11:49:58 +0400

    CityEdit completing

--- MS_Access/BtnForms/FormEditCity.Designer.cs
+++ MS_Access/BtnForms/FormEditCity.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCancelEdit = new System.Windows.Forms.Button();
+            this.btnSaveEdit = new System.Windows.Forms.Button();
+            this.txtCityNameEdit = new System.Windows.Forms.TextBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.groupboxEditCity = new System.Windows.Forms.GroupBox();
+            this.groupboxEditCity.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCancelEdit
+            // 
+            this.btnCancelEdit.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnCancelEdit.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelEdit.Location = new System.Drawing.Point(275, 203);
+            this.btnCancelEdit.Name = "btnCancelEdit";
+            this.btnCancelEdit.Size = new System.Drawing.Size(113, 37);
+            this.btnCancelEdit.TabIndex = 14;
+            this.btnCancelEdit.Text = "Չեղարկել";
+            this.btnCancelEdit.UseVisualStyleBackColor = false;
+            this.btnCancelEdit.Click += new System.EventHandler(this.btnCancelEdit_Click);
+            // 
+            // btnSaveEdit
+            // 
+            this.btnSaveEdit.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnSaveEdit.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveEdit.Location = new System.Drawing.Point(99, 203);
+            this.btnSaveEdit.Name = "btnSaveEdit";
+            this.btnSaveEdit.Size = new System.Drawing.Size(113, 37);
+            this.btnSaveEdit.TabIndex = 13;
+            this.btnSaveEdit.Text = "Գրանցել";
+            this.btnSaveEdit.UseVisualStyleBackColor = false;
+            this.btnSaveEdit.Click += new System.EventHandler(this.btnSaveEdit_Click);
+            // 
+            // txtCityNameEdit
+            // 
+            this.txtCityNameEdit.Location = new System.Drawing.Point(203, 71);
+            this.txtCityNameEdit.Multiline = true;
+            this.txtCityNameEdit.Name = "txtCityNameEdit";
+            this.txtCityNameEdit.Size = new System.Drawing.Size(157, 30);
+            this.txtCityNameEdit.TabIndex = 9;
+            // 
+            // lblCity
+            // 
+            this.lblCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(-3, 71);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(183, 30);
+            this.lblCity.TabIndex = 8;
+            this.lblCity.Text = "Քաղաք/մարզ անվանում";
+            this.lblCity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupboxEditCity
+            // 
+            this.groupboxEditCity.Controls.Add(this.txtCityNameEdit);
+            this.groupboxEditCity.Controls.Add(this.lblCity);
+            this.groupboxEditCity.Location = new System.Drawing.Point(72, 29);
+            this.groupboxEditCity.Name = "groupboxEditCity";
+            this.groupboxEditCity.Size = new System.Drawing.Size(366, 168);
+            this.groupboxEditCity.TabIndex = 12;
+            this.groupboxEditCity.TabStop = false;
+            // 
+            // FormEditCity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormEditCity";
+            this.ClientSize = new System.Drawing.Size(506, 260);
+            this.Controls.Add(this.btnCancelEdit);
+            this.Controls.Add(this.btnSaveEdit);
+            this.Controls.Add(this.groupboxEditCity);
+            this.Name = "FormEditCity";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Խմբագրել";
+            this.groupboxEditCity.ResumeLayout(false);
+            this.groupboxEditCity.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCancelEdit;
+        private System.Windows.Forms.Button btnSaveEdit;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.GroupBox groupboxEditCity;
+        public System.Windows.Forms.TextBox txtCityNameEdit;
     }
 }
--- MS_Access/BtnForms/FormEditCity.cs
+++ MS_Access/BtnForms/FormEditCity.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MS_Access.InformerForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,34 @@ namespace MS_Access.BtnForms
 {
     public partial class FormEditCity : Form
     {
+        OleDbCommand command;
+        public static FormEditCity instancecity;
         public FormEditCity()
         {
             InitializeComponent();
+            instancecity = this;
+            txtCityNameEdit.Text = CityProvince.instancecity.GetCityName();
+        }
+
+        private void btnSaveEdit_Click(object sender, EventArgs e)
+        {
+            string updateQuery = "UPDATE City SET City.City =" + " @cityname " + "WHERE (id = @id);";
+            command = new OleDbCommand(updateQuery, FormMain._instance.oledbconstring);
+            command.Parameters.AddWithValue("@cityname", txtCityNameEdit.Text);
+            command.Parameters.AddWithValue("@id", Convert.ToInt32(CityProvince.instancecity.dgvCity.CurrentRow.Cells[0].Value.ToString()));
+            FormMain._instance.oledbconstring.Open();
+            command.ExecuteNonQuery();
+            FormMain._instance.oledbconstring.Close();
+            MessageBox.Show("Տվյալները թարմացված են");
+            CityProvince.instancecity.tsbUpdateCity_Click(null, null);
+            this.Close();
+            txtCityNameEdit.Clear();
+        }
+
+        private void btnCancelEdit_Click(object sender, EventArgs e)
+        {
+            txtCityNameEdit.Clear();
+            this.Close();
         }
     }
 }
